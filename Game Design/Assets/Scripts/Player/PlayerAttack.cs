521acc3075855ed12464498180a213bb91b9c639
Vladimir G <159206796+Vladimir-Grebenets@users.noreply.github.com>
2025-08-05 19:58:37 +1000

    Update PlayerAttack.cs

--- Game Design/Assets/Scripts/Player/PlayerAttack.cs
+++ Game Design/Assets/Scripts/Player/PlayerAttack.cs
@@ -6,13 +6,21 @@ public class PlayerAttack : MonoBehaviour
 {
     public float bounceForce = 5f;
 
-    void OnTriggerEnter2D(Collider2D other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Enemy"))
-        {
-            other.GetComponent<enemyHealth>().TakeDamage(1f);
-            GetComponentInParent<Rigidbody2D>().velocity = Vector2.zero;
-            GetComponentInParent<Rigidbody2D>().AddForce(Vector2.up * bounceForce, ForceMode2D.Impulse);
-        }
+        if (!other.CompareTag("Weak"))
+            return;
+
+        enemyHealth enemy = other.GetComponentInParent<enemyHealth>();
+        if (enemy == null) return;
+
+        enemy.TakeDamage(1f);
+
+        Rigidbody2D rb = GetComponentInParent<Rigidbody2D>();
+        if (rb == null) return;
+
+        rb.velocity = Vector2.zero;
+        rb.AddForce(Vector2.up * bounceForce, ForceMode2D.Impulse);
     }
+
 }
