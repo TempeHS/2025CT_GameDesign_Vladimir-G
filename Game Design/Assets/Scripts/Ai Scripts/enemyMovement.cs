ec825cfe6e79238d4d294978976d10e47fab13cd
Vladimir G <159206796+Vladimir-Grebenets@users.noreply.github.com>
2025-08-05 19:39:19 +1000

    enemy done

--- Game Design/Assets/Scripts/Ai Scripts/enemyMovement.cs
+++ Game Design/Assets/Scripts/Ai Scripts/enemyMovement.cs
@@ -6,8 +6,12 @@ public class enemyMovement : MonoBehaviour
 {
     public Transform[] partrolPoints;
     public float speed;
+    public float chaseSpeed;
     private int patrolDestination;
     private Animator animator;
+    public Transform playerTransform;
+    public bool isChasing;
+    public float chaseDistance;
 
     void Awake()
     {
@@ -17,25 +21,52 @@ public class enemyMovement : MonoBehaviour
 
     void Update()
     {
-        if (patrolDestination == 0)
+        if (isChasing)
         {
             animator.SetBool("isRunning", true);
-            transform.position = Vector2.MoveTowards(transform.position, partrolPoints[0].position, speed * Time.deltaTime);
-            if (Vector2.Distance(transform.position, partrolPoints[0].position) < 0.5f)
+            if (playerTransform.position.x > transform.position.x)
             {
+                transform.position += Vector3.right * chaseSpeed * Time.deltaTime;
                 transform.localScale = new Vector3(7, 7, 7);
-                patrolDestination = 1;
+            }
+            else
+            {
+                transform.position += Vector3.left * chaseSpeed * Time.deltaTime;
+                transform.localScale = new Vector3(-7, 7, 7);
             }
         }
-        else if (patrolDestination == 1)
+        else
         {
-            animator.SetBool("isRunning", true);
-            transform.position = Vector2.MoveTowards(transform.position, partrolPoints[1].position, speed * Time.deltaTime);
-            if (Vector2.Distance(transform.position, partrolPoints[1].position) < 0.5f)
+            if (Vector2.Distance(transform.position, playerTransform.position) < chaseDistance)
             {
-                transform.localScale = new Vector3(-7, 7, 7);
-                patrolDestination = 0;
+                isChasing = true;
+                animator.SetBool("isRunning", true);
+            }
+
+
+            if (patrolDestination == 0)
+            {
+                animator.SetBool("isRunning", true);
+                transform.position = Vector2.MoveTowards(transform.position, partrolPoints[0].position, speed * Time.deltaTime);
+                if (Vector2.Distance(transform.position, partrolPoints[0].position) < 0.5f)
+                {
+                    transform.localScale = new Vector3(7, 7, 7);
+                    patrolDestination = 1;
+                }
+            }
+            else if (patrolDestination == 1)
+            {
+                animator.SetBool("isRunning", true);
+                transform.position = Vector2.MoveTowards(transform.position, partrolPoints[1].position, speed * Time.deltaTime);
+                if (Vector2.Distance(transform.position, partrolPoints[1].position) < 0.5f)
+                {
+                    transform.localScale = new Vector3(-7, 7, 7);
+                    patrolDestination = 0;
+                }
             }
         }
     }
 }
+
+// playerTransform.position += Vector3.right * chaseSpeed * Time.deltaTime;
+// works like black hole
