b366622693e4de79122def96cad78a199de24085
Vladimir G <159206796+Vladimir-Grebenets@users.noreply.github.com>
2025-08-13 12:56:56 +1000

    started wirjk

--- Game Design/Assets/Scripts/Boss/BossController.cs
+++ Game Design/Assets/Scripts/Boss/BossController.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class BossController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float speed = 4;
+    public float chaseSpeed = 8;
+    public float chaseDuration = 4f;
+    private Animator animator;
+    public Transform playerTransform;
+
+
+    void Awake()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public void BossChase()
     {
         
     }
+    
+
+    public void BossFlee()
+    {
+
+    }
+
+
+    public void PickAction()
+    {
+        int randomNumber = Random.Range(0, 1);
+
+        if (randomNumber == 0)
+        {
+            BossChase();
+        }
+        else
+        {
+            BossFlee();
+        }
+    }
+
+
+    public void Update()
+    {
+
+    }
 }
+
